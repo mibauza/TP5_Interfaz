1d7fc3b0fff189a92a50c532f71c28f731efa1f2
unknown <figuta@gmail.com>
2024-11-17 17:17:58 -0300

    Solucion a errores en app.component.ts

--- Voluntarias_Animalistas_Necochea/src/app/app.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/app.component.ts
@@ -4,15 +4,21 @@ import { HeaderComponent } from "./componentes/header/header.component";
 import { CarruselComponent } from "./componentes/carrusel/carrusel.component";
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
-import { HeaderComponent_1 as HeaderComponent } from "./componentes/header/header.component";
-import { CarruselComponent_1 as CarruselComponent } from "./componentes/carrusel/carrusel.component";
+//import { HeaderComponent_1 as HeaderComponent } from "./componentes/header/header.component";
+//import { CarruselComponent_1 as CarruselComponent } from "./componentes/carrusel/carrusel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PrincipalComponent, NosotrosComponent, CarruselComponent, HeaderComponent_1, CarruselComponent_1],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    PrincipalComponent,
+    NosotrosComponent,
+    CarruselComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Voluntarias_Animalistas_Necochea';

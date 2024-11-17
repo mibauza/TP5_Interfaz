1815bcd9ba9a15f392a88c05ea0e9343b8250654
mibauza <matiasibauza@gmail.com>
2024-11-17 12:20:22 -0300

    1

--- Voluntarias_Animalistas_Necochea/src/app/app.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "./componentes/header/header.component";
 import { CarruselComponent } from "./componentes/carrusel/carrusel.component";
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { HeaderComponent_1 as HeaderComponent } from "./componentes/header/header.component";
+import { CarruselComponent_1 as CarruselComponent } from "./componentes/carrusel/carrusel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PrincipalComponent,NosotrosComponent, CarruselComponent],
+  imports: [RouterOutlet, HeaderComponent, PrincipalComponent, NosotrosComponent, CarruselComponent, HeaderComponent_1, CarruselComponent_1],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

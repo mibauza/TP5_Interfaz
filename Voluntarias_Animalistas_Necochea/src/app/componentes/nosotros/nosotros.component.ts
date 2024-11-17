73c243b9a02ebd6c92b67a014e07a58b844f7081
unknown <figuta@gmail.com>
2024-11-17 16:46:52 -0300

    Agregue rutas a los TS de los modulos

--- Voluntarias_Animalistas_Necochea/src/app/componentes/nosotros/nosotros.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/componentes/nosotros/nosotros.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
--- Voluntarias_Animalistas_Necochea/src/app/componentes/principal/principal.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/componentes/principal/principal.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CarruselComponent } from "../carrusel/carrusel.component";
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CarruselComponent, FooterComponent, NavbarComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })

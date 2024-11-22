16ba88b93f6928dea65f801928836321f0f357ba
mibauza <matiasibauza@gmail.com>
2024-11-22 00:31:19 -0300

    ñakdfngkjd

--- Voluntarias_Animalistas_Necochea/src/app/componentes/carrusel/carrusel.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/componentes/carrusel/carrusel.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrusel',
   standalone: true,
-  imports: [NgFor,CommonModule],
+  imports: [CommonModule],
   templateUrl: './carrusel.component.html',
   styleUrl: './carrusel.component.css'
 })
--- Voluntarias_Animalistas_Necochea/src/app/componentes/home/home.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/componentes/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CarruselComponent } from "../carrusel/carrusel.component";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarruselComponent],
+  imports: [CarruselComponent, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  elementos = [
+    { img: '../../assets/animales/amiguitos.jpeg', texto: 'Primer elemento' },
+    { img: '../../assets/animales/perro1.jpg', texto: 'Segundo elemento' },
+    { img: '../../assets/animales/perro2.jpg', texto: 'Tercer elemento' }
+  ];
 }

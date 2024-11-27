471105b07ee69009f7dc6ab737c3842024433d8a
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2024-11-27 19:24:14 -0300

    Mejoras en el Home

--- Voluntarias_Animalistas_Necochea/src/app/componentes/home/home.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/componentes/home/home.component.ts
@@ -13,10 +13,6 @@ export class HomeComponent {
   elementos = [
     { img: '../../assets/animales/img1.jpeg', texto: 'Ley 14.346 de “Maltrato animal” y la Ley Nacional de Protección de Animales N° 2786, conocida como “Ley Sarmiento”. ¿Cómo denunciar?' },
     { img: '../../assets/animales/img2.jpeg', texto: 'Adoptá con Amor y Responsabilidad y siempre tendrás un corazón latiendo por ti, esperándote en casa.' },
-    { img: '../../assets/animales/img3.jpeg', texto: 'En Argentina, se estima que más del 60% de las mascotas en los hogares son adoptadas.' },
-    { img: '../../assets/animales/img4.jpg', texto: 'Ley 14.346 de “Maltrato animal” y la Ley Nacional de Protección de Animales N° 2786, conocida como “Ley Sarmiento”. ¿Cómo denunciar?' },
-    { img: '../../assets/animales/img5.jpg', texto: 'Adoptá con Amor y Responsabilidad y siempre tendrás un corazón latiendo por ti, esperándote en casa.' },
-    { img: '../../assets/animales/img6.jpg', texto: 'En Argentina, se estima que más del 60% de las mascotas en los hogares son adoptadas.' }
-  
+    { img: '../../assets/animales/img3.jpeg', texto: 'En Argentina, se estima que más del 60% de las mascotas en los hogares son adoptadas.' }
   ];
 }

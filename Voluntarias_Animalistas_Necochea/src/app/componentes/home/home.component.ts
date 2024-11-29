73311dd9893bb96de65f26e1c373e03cc16370d6
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2024-11-28 21:03:54 -0300

    Mejoras estilo e imagenes

--- Voluntarias_Animalistas_Necochea/src/app/componentes/home/home.component.ts
+++ Voluntarias_Animalistas_Necochea/src/app/componentes/home/home.component.ts
@@ -11,7 +11,7 @@ import { NgFor } from '@angular/common';
 })
 export class HomeComponent {
   elementos = [
-    { img: '../../assets/animales/img1.jpeg', texto: 'Ley 14.346 de “Maltrato animal” y la Ley Nacional de Protección de Animales N° 2786, conocida como “Ley Sarmiento”. ¿Cómo denunciar?' },
+    { img: '../../assets/animales/img1.jpeg', texto: 'Ley 14.346 de “Maltrato animal” y la Ley Nacional de Protección de Animales N° 2786, conocida como “Ley Sarmiento”.' },
     { img: '../../assets/animales/img2.jpeg', texto: 'Adoptá con Amor y Responsabilidad y siempre tendrás un corazón latiendo por ti, esperándote en casa.' },
     { img: '../../assets/animales/img3.jpeg', texto: 'En Argentina, se estima que más del 60% de las mascotas en los hogares son adoptadas.' }
   ];
